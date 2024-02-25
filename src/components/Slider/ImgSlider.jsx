6251f50144fa487dba9ef890753d5abd824596df
Shivankit Agarwal <shivankit1210@gmail.com>
2024-02-25 23:39:23 +0530

    slider updated

--- src/components/Slider/ImgSlider.jsx
+++ src/components/Slider/ImgSlider.jsx
@@ -3,9 +3,6 @@ import React from "react";
 function ImgSlider() {
     return (
         <div>
-            <h1 class="text-5xl font-bold bg-red-500">
-    Hello world!
-  </h1>
   <h1>Slider</h1>
         </div>
        
