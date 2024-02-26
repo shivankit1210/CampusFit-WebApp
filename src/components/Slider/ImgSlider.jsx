6256025b5e414aeccf9664d83e037c45e4a169c0
Shivankit Agarwal <shivankit1210@gmail.com>
2024-02-26 23:53:42 +0530

    updated

--- src/components/Slider/ImgSlider.jsx
+++ src/components/Slider/ImgSlider.jsx
@@ -5,11 +5,47 @@
 //         <div>
 //   <h1>Slider</h1>
 //         </div>
-       
-        
 
 //     );
 // }
 
 // export default ImgSlider;
 
+import React, { useState } from "react";
+import "./ImageSlider.css"; // Import CSS for styling
+
+const ImgSlider = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className="slider">
+      <button className="prev" onClick={prevSlide}>
+        &#10094;
+      </button>
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={index === currentIndex ? "slide active" : "slide"}
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+      ))}
+      <button className="next" onClick={nextSlide}>
+        &#10095;
+      </button>
+    </div>
+  );
+};
+
+export default ImgSlider;
