c48ab8f12313ec82a82b9892ac5927230c498378
Shivankit Agarwal <shivankit1210@gmail.com>
2024-02-26 23:55:10 +0530

    added images to slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import ImgSlider from "./components/Slider/ImgSlider";
 import Navbar from "./components/navbar/Navbar";
+const App = () => {
+  const images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    // Add more image URLs as needed
+  ];
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ImgSlider />
+      <ImgSlider images={images} />
     </div>
   );
 }
