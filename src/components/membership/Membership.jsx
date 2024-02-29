4dabb557d532655472ad624b576fe01154a8e7f0
Shivankit Agarwal <shivankit1210@gmail.com>
2024-03-01 04:33:22 +0530

    add photo in membership

--- src/components/membership/Membership.jsx
+++ src/components/membership/Membership.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./membership.css";
+import membership from "../../images/membership.jpg";
 
- const Membership = () =>{
-    return (
-<div className="membership">
-    <div className="left">
+const Membership = () => {
+  return (
+    <div className="membership">
+      <div className="left">
+        <h1>MEMBERSHIP</h1>
+        <p>
+          "Join the IT Gopeshwar Gym community today and take the first step
+          towards a healthier lifestyle! With a monthly membership fee of just
+          100 rupees, access state-of-the-art facilities, expert guidance, and a
+          supportive environment to help you reach your fitness goals. Plus, for
+          new members, there's an exciting one-time registration fee of only 250
+          rupees. Don't miss out on this incredible opportunity to invest in
+          your health and well-being. Sign up now and start your fitness journey
+          with us!"
+        </p>
+      </div>
+      <div className="right">
+        <img src={membership} alt="" />
+      </div>
+    </div>
+  );
+};
 
-    </div>
-    <div className="right">
-        
-    </div>
-</div>
-    )
- }
-  
- export default Membership;
+export default Membership;
